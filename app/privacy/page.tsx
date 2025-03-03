c6874e8f074cee7ec0dc37a1992d7bef24d3e374
Stanley Kariuki <103861317+leestan306@users.noreply.github.com>
2025-03-03 21:01:05 +0300

    remove bg slate

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -9,7 +9,7 @@ const PrivacyPolicyPage = () => {
   });
 
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center py-10">
+    <div className=" min-h-screen flex items-center justify-center py-10">
       <div className="max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-lg">
         <h1 className="text-3xl font-bold text-center mb-6">Privacy Policy</h1>
         <p className="text-sm text-gray-500 text-center mb-6">
