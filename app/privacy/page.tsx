40e058e37e6d2fc45667d4023e77e4f3658eb7cd
Stanley Kariuki <103861317+leestan306@users.noreply.github.com>
2025-03-03 21:02:55 +0300

    remove bg white

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -10,7 +10,7 @@ const PrivacyPolicyPage = () => {
 
   return (
     <div className=" min-h-screen flex items-center justify-center py-10">
-      <div className="max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-lg">
+      <div className="max-w-4xl mx-auto p-6 shadow-lg rounded-lg">
         <h1 className="text-3xl font-bold text-center mb-6">Privacy Policy</h1>
         <p className="text-sm text-gray-500 text-center mb-6">
           Last Updated: <span className="font-medium">{lastUpdatedDate}</span>
